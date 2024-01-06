ac790c6986531d799a64a2250b9e3a1cce2f5a1a
Radhe Shyam <154522052+RADHE-SHYAM-03@users.noreply.github.com>
2024-01-06 23:02:07 +0530

    Binary Search.cpp

--- Arrays/Binary Search.cpp
+++ Arrays/Binary Search.cpp
@@ -1,34 +1,23 @@
-#include <iostream>
-using namespace std;
-int binarysearch(int arr[],int n,int key){
-	int start=0;
-	int end=n-1;
-	int mid=end-(start+end)/2;
-	while(start<=end){
-		if(arr[mid]==key){
-			return mid;
-		}
-		else if (arr[mid]<=key){
-			start=mid+1;
-		}
-		else{
-			end=mid-1;
-		}
-		mid=end-(start+end)/2;
-	}
-	return -1;
-}
- int main()
- {
- 	int arr[10]={1,2,3,4,5,6,7,8,9,100};
- 	cout<<"The arrays elements are : ";
- 	for(int i=0;i<10;i++)
- 	{
- 		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
- 	int key;
- 	cout<<"Enter the element to find ?"<<endl;
- 	cin>>key;
- 	cout<<"Element is at Index :"<<binarysearch(arr,10,key);
-}
+class Solution {
+public:
+    int search(vector<int>& arr, int k) {
+     int n = arr.size()-1;
+     int s=0;
+     int  e=n;
+     int mid=s+(e-s)/2;
+    while(s<=e){
+         if(arr[mid]==k){
+             return mid;
+         }
+         if(arr[mid]>k){
+             e=mid-1;
+         }
+         else{
+             s=mid+1;
+         }
+         mid=s+(e-s)/2;
+        
+     }
+        return -1;
+    }
+};
